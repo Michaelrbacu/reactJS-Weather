391462c450d901d80c04f428033848e71a9a8a81
michael <michaelrbacu2@gmail.com>
2023-07-03 06:29:08 -0400

    added button for weather app + placeholder button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const api = {
   key: "4f8e795dcd6dbf7b9f5276bff095ffc1",
   base: "https://api.openweathermap.org/data/2.5/"
-}
+};
 
 const popularCities = [
   "London",
@@ -21,23 +21,20 @@ function App() {
   const [weather, setWeather] = useState({});
   const [showDropdown, setShowDropdown] = useState(false);
 
-  const search = (evt) => {
-    if (evt.key === "Enter") {
-      setShowDropdown(false);
-      fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
-        .then((res) => res.json())
-        .then((result) => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
-    }
+  const search = () => {
+    setShowDropdown(false);
+    fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });
   };
 
   const handleDropdownClick = (city) => {
     setQuery(city);
     setShowDropdown(false);
-    
   };
 
   const dateBuilder = (d) => {
@@ -81,15 +78,25 @@ function App() {
     <div className={(typeof weather.main !== "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-            onFocus={handleSearchBarFocus}
-          />
+          <div className="search-bar-container">
+            <input 
+              type="text"
+              className="search-bar"
+              placeholder="Search..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              onFocus={handleSearchBarFocus}
+            />
+            <button className="search-button" onClick={search}>
+              Search
+            </button>
+            <button className="search-button" onClick={search}>
+              Weekly
+            </button>
+            <button className="search-button" onClick={search}>
+              Monthly
+            </button>
+          </div>
           {showDropdown && (
             <div className="dropdown">
               {popularCities.map((city, index) => (
@@ -105,18 +112,18 @@ function App() {
           )}
         </div>
         {(typeof weather.main !== "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°c
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
           </div>
-        </div>
         ) : ('')}
       </main>
     </div>
